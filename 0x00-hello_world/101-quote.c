22a43693f3e83097007bb2af45cca26d4ae870e6
Yannick Luther Agbana <99275029+YannickAgbana@users.noreply.github.com>
2022-03-11 03:39:13 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 /**
- *main - The standard error will print a certain output
- *Return: The value will return 1
- */
-
+*main - The standard error will print a certain output
+*Return: The value will return 1
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
