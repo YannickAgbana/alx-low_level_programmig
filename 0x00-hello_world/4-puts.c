dd7707c3011f0483912c46ecb86bc775c3c0cf9d
Yannick Luther Agbana <99275029+YannickAgbana@users.noreply.github.com>
2022-03-11 01:12:30 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
 * main - Entry point
-*
+* Return: Always 0 (Success)
 *Description: using the main function
 *this program prints "Programming is like building a multilingual puzzle
-*Return: 0
 */
 int main(void)
 {
